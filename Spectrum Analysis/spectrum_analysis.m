224fb2ea3d33950687dd0f2c12693aa4eb03e198
o7 <51698735+nqganh-03@users.noreply.github.com>
2025-04-20 10:40:10 +0700

    Create spectrum_analysis.m

---
+++ Spectrum Analysis/spectrum_analysis.m
@@ -0,0 +1,17 @@
+[audio, fs] = audioread('group_record.wav');
+audio = mean(audio, 2);         
+N = length(audio);
+f = (0:N-1) * (fs/N);             % Accurate frequency axis
+Y = fft(audio);
+magnitude = abs(Y) / N;           % Normalize magnitude
+
+half_N = floor(N/2);              % First half (positive frequencies)
+f_plot = f(1:half_N);
+magnitude_plot = magnitude(1:half_N);
+
+plot(f_plot, 20*log10(magnitude_plot + eps));
+xlabel('Frequency (Hz)');
+ylabel('Magnitude (dB)');
+title('Spectrum of Recorded Audio');
+grid on;
+xlim([0 fs/2]);
